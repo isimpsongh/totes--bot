f9ccbe90bf5ef89a0835c71f8758227646a05c9e
Isaiah Simpson <isimpson@grubhub.com>
2014-07-30 07:41:14 -0500

    stuff

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 'use strict';
 
 var DEFAULT_PORT = process.env.PORT || 5000,
+    THE_YURT_POST_URL = 'https://api.hipchat.com/v2/room/98025/notification',
+    YURT_FATHER_POST_URL = 'https://api.hipchat.com/v2/user/15664/message',
     YAHOO_GRUB_QUOTE_URL = 'http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol in ("GRUB")&env=http://datatables.org/alltables.env&format=json',
     express = require('express'),
     request = require('request'),
@@ -25,13 +27,24 @@ if (argv.help) {
     return;
 }
 
+function getJacksFerrariFactor (ret) {
+    request(YAHOO_GRUB_QUOTE_URL, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        ret('' + Math.floor(parseFloat(JSON.parse(body).query.results.quote.BidRealtime) * 0.36));
+      }
+    });  
+}
+
 var app = express()
-        .get('/publishJacksFerarriReport', function (req, res) {
-            request(YAHOO_GRUB_QUOTE_URL, function (error, response, body) {
-              if (!error && response.statusCode == 200) {
-                
-              }
-            });
+        .get('/', function(req, res) {
+          res.send('YurtChat is running.');
+        })
+        .get('/jacksFerrariFactor', function (req, res) {
+            getJacksFerrariFactor(res.send);
+        })
+        .get('/postJacksFerrariReport', function (req, res) {
+            var ferrariFactor = getJacksFerrariFactor();
+            request()       
         });
 
 app.listen(argv.port);
